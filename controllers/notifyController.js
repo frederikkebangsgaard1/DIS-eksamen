5369d9afa2ed7653028beb35e80b4e9cf22dc922
FrederikkeBangsgaard <Frederikke.bangsgaard@gmail.com>
2025-11-30 13:19:15 +0100

    Gigachad

--- controllers/notifyController.js
+++ controllers/notifyController.js
@@ -52,7 +52,7 @@ async function sendEmail(to, subject, text) {
 async function sendSMS(to, body) {
   return await twilioClient.messages.create({
     body: body,
-    from: 'Weather Buddy',
+    from: 'DIS Weather Buddy',
     to: "+" +to
   });
 }
